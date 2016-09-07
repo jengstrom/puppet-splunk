d553d66b52f084d697ba1d25c4068f829732efb2
Jeff Engstrom <jeff.engstrom@nike.com>
2016-09-07 13:18:31 -0700

    updates web to include new webssl value

--- manifests/web.pp
+++ manifests/web.pp
@@ -7,7 +7,8 @@ class splunk::web (
   $splunk_os_user = $splunk::splunk_os_user,
   $splunk_app_precedence_dir = $splunk::splunk_app_precedence_dir,
   $splunk_app_replace = $splunk::splunk_app_replace,
-  $splunk_home = $splunk::splunk_home
+  $splunk_home = $splunk::splunk_home,
+  $webssl = $splunk::webssl
 ){
   $splunk_app_name = 'puppet_common_ssl_web'
   if $httpport == undef {
